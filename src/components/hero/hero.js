a78233f3272d487fcd11dd472c47fc3938a1b1d2
Anastasia Prom tep <apromtep@gmail.com>
2023-03-12 20:48:27 +0100

    Add hero component

---
+++ src/components/hero/hero.js
@@ -0,0 +1,14 @@
+import {StaticImage} from "gatsby-plugin-image";
+import * as React from "react";
+
+const Hero = () => {
+    return (
+        <main>
+            <StaticImage src='../../images/placeholder.svg' alt='Place holder image'/>
+            <span>L'alchimie de votre contenu</span>
+        </main>
+
+    )
+}
+
+export default Hero
