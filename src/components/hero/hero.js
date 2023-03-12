3312e6fcaa238f15f8c02477eaf1447b70bc8f40
Anastasia Prom tep <apromtep@gmail.com>
2023-03-12 21:52:28 +0100

    Add hero image

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -5,7 +5,7 @@ import { container, image, title } from "./hero.module.css"
 const Hero = () => {
     return (
         <main className={container}>
-            <StaticImage className={image} src='../../images/placeholder.svg' alt='Place holder image'/>
+            <StaticImage className={image} src='../../images/hero.jpeg' alt='Place holder image'/>
             <span className={title}>L'alchimie de votre contenu</span>
         </main>
     )
