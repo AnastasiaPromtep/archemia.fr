3ade5252189c6bdc169c6597ae95ec64911f7475
Anastasia Prom tep <apromtep@gmail.com>
2023-03-12 21:19:58 +0100

    Add styling to hero component

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,13 +1,13 @@
 import {StaticImage} from "gatsby-plugin-image";
 import * as React from "react";
+import { container, image, title } from "./hero.module.css"
 
 const Hero = () => {
     return (
-        <main>
-            <StaticImage src='../../images/placeholder.svg' alt='Place holder image'/>
-            <span>L'alchimie de votre contenu</span>
+        <main className={container}>
+            <StaticImage className={image} src='../../images/placeholder.svg' alt='Place holder image'/>
+            <span className={title}>L'alchimie de votre contenu</span>
         </main>
-
     )
 }
 
