328145a457bdb8d0439d0aef964e3709cc8d772e
Anastasia Prom tep <apromtep@gmail.com>
2023-04-02 22:38:18 +0200

    Add new pages to navigation links

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,15 +1,18 @@
 import * as React from "react";
 import { container, navigationLink, link, title } from "./navigation.module.css";
+import {Link} from "gatsby";
 
 const Navigation = () => {
     return (
         <main className={ container }>
-            <h1 className={ title }>Archemia</h1>
+            <h1 className={ title }>
+                <Link to='/'>Archemia</Link>
+            </h1>
             <nav className={ navigationLink }>
                 <a className={ link } href="#">Galerie</a>
                 <a className={ link } href="#">Projets</a>
-                <a className={ link } href="#">Blog</a>
-                <a className={ link } href="#">Qui sommes nous ?</a>
+                <Link to='/blog' className={ link }>Blog</Link>
+                <Link to='/about' className={ link }>About</Link>
             </nav>
         </main>
     )
