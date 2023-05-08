a181c5bf9401f9136cd7618fb1d99a7eab25ef1a
Anastasia Prom tep <apromtep@gmail.com>
2023-05-08 09:54:59 +0200

    Add transparency to nav bar on hero component

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,18 +1,39 @@
 import * as React from "react";
-import { container, navigationLink, link, title } from "./navigation.module.css";
+import { container, navigationLink, link, title, activeContainer } from "./navigation.module.css";
 import {Link} from "gatsby";
+import {StaticImage} from "gatsby-plugin-image";
+import {useEffect, useState} from "react";
 
 const Navigation = () => {
+    const [isNavBarTransparent, setIsNavBarTransparent] = useState(true);
+    const changeBackground = () => {
+        if (window.scrollY >= 100) {
+            setIsNavBarTransparent(false);
+        } else {
+            setIsNavBarTransparent(true);
+        }
+    }
+
+    useEffect(() => {
+        changeBackground();
+        window.addEventListener('scroll', changeBackground);
+
+        return () => {
+            window.removeEventListener('scroll', changeBackground);
+        }
+    });
+
     return (
-        <main className={ container }>
+        <main className={ isNavBarTransparent ? container : activeContainer }>
             <h1 className={ title }>
-                <Link to='/'>Archemia</Link>
+                <Link to='/'>
+                    <StaticImage src={'../../images/logo.png'} alt="Logo" width={50}/>
+                </Link>
             </h1>
             <nav className={ navigationLink }>
-                <a className={ link } href="#">Galerie</a>
                 <a className={ link } href="#">Projets</a>
                 <Link to='/blog' className={ link }>Blog</Link>
-                <Link to='/about' className={ link }>About</Link>
+                <Link to='/about' className={ link }>Qui sommes-nous ?</Link>
             </nav>
         </main>
     )
