2b2c80262ab138012e96ab7a89cf9782a4922f28
Anastasia Prom tep <apromtep@gmail.com>
2023-05-13 20:16:33 +0200

    Add minimum style to illustrated example

--- src/components/illustrated-example/illustrated-example.js
+++ src/components/illustrated-example/illustrated-example.js
@@ -15,6 +15,7 @@ const IllustratedExample = ({left = false}) => {
                     Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
                     magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.
                 </p>
+                <button>Voir la formule</button>
             </div>
         </main>
     )
