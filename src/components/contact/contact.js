144f7bcde0f29ed10d23dcb84ceffe1449d3761e
Anastasia Prom tep <apromtep@gmail.com>
2023-05-13 19:52:56 +0200

    Add little spacing to contact preview

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { container } from './contact.module.css';
+import { container, textContainer } from './contact.module.css';
 
 const Contact = () => {
     return (
         <main className={ container }>
-            <div>
+            <div className={ textContainer }>
                 <h1>Un projet ? Parlons-nous !</h1>
                 <h2>Etablissons ensemble les composants de formules</h2>
                 <button>Contact</button>
