7c307c4befc3b57b7a8c1130de3948516de87c21
Anastasia Prom tep <apromtep@gmail.com>
2023-05-14 18:33:58 +0200

    Restyle whole animated logo section
    Add a responsive design

--- src/components/animated-logo/animated-logo.js
+++ src/components/animated-logo/animated-logo.js
@@ -1,66 +1,56 @@
 import * as React from "react";
-import { container,
-    cardVideo,
-    cardEditor,
-    cardWeb,
+import {
+    container,
     littleCircleVideo,
     littleCircleWeb,
     littleCircleEditor,
     iconVideo,
     iconEditor,
-    iconWeb} from './animated-logo.module.css';
+    iconWeb,
+    alchemy,
+    textContainer,
+    textBox,
+    logoContainer
+} from './animated-logo.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVideo, faPen, faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 const AnimatedLogo = () => {
   return (
       <main id='animatedLogo' className={ container }>
-          <div className={ cardVideo }>
-              <div className={ littleCircleVideo }>
-                  <FontAwesomeIcon icon={ faVideo} className={iconVideo} />
+          <div className={ alchemy }>
+              <div className={ logoContainer }>
+                  <div className={ littleCircleVideo }>
+                      <FontAwesomeIcon icon={ faVideo} className={iconVideo} />
+                  </div>
+                  <div className={ littleCircleEditor }>
+                      <FontAwesomeIcon icon={faPen} className={ iconEditor }/>
+                  </div>
+                  <div className={ littleCircleWeb }>
+                      <FontAwesomeIcon icon={faGlobe} className={ iconWeb }/>
+                  </div>
               </div>
-              <p>
-                  Nous façonnons votre contenu pour qu'ils soit incroyable. ifzeoijfoij foizejfoizejf ozejf oizje foijz
-                  ofjzoijfz ozj ofizej foizjef ozijf oizej oizje foijze fozje
-                  Nous façonnons votre contenu pour qu'ils soit incroyable. ifzeoijfoij foizejfoizejf ozejf oizje foijz
-                  ofjzoijfz ozj ofizej foizjef ozijf oizej oizje foijze fozje
-                  Nous façonnons votre contenu pour qu'ils soit incroyable. ifzeoijfoij foizejfoizejf ozejf oizje foijz
-                  ofjzoijfz ozj ofizej foizjef ozijf oizej oizje foijze fozje
-                  Nous façonnons votre contenu pour qu'ils soit incroyable.
-              </p>
+              <div className={ textContainer }>
+                  <div className={ textBox }>
+                      <h1>Vidéo</h1>
+                      <h2>Un truc important</h2>
+                      <p>
+                          On vous fait des vidéo aux petits oignions (bien que Calvin déteste ça)
+                          et un rendu super quali avec un grand K. Interview, barmitzvah, film triple A, on s'occupe de tout.
+                          En plus Thomas est un dorniste cetifié, dans le domaine on l'appelle le Tom Cruise de la
+                          Ford Fiesta. Il même appelé son drone Fuego!. Et il peut même le piloter en VR.
+                      </p>
+                      <button>
+                          Voir des video cool
+                      </button>
+                  </div>
+              </div>
           </div>
 
-          <div className={ cardEditor }>
-              <div className={ littleCircleEditor }>
-                  <FontAwesomeIcon icon={faPen} className={ iconEditor }/>
-              </div>
-              <p>
-                  Nous façonnons votre contenu pour qu'ils soit incroyable. ifzeoijfoij foizejfoizejf ozejf oizje foijz
-                  ofjzoijfz ozj ofizej foizjef ozijf oizej oizje foijze fozje
-                  Nous façonnons votre contenu pour qu'ils soit incroyable. ifzeoijfoij foizejfoizejf ozejf oizje foijz
-                  ofjzoijfz ozj ofizej foizjef ozijf oizej oizje foijze fozje
-                  Nous façonnons votre contenu pour qu'ils soit incroyable. ifzeoijfoij foizejfoizejf ozejf oizje foijz
-                  ofjzoijfz ozj ofizej foizjef ozijf oizej oizje foijze fozje
-                  Nous façonnons votre contenu pour qu'ils soit incroyable.
-              </p>
-          </div>
-
-          <div className={ cardWeb }>
-              <div className={ littleCircleWeb }>
-                  <FontAwesomeIcon icon={faGlobe} className={ iconWeb }/>
-              </div>
-              <p>
-                  Nous façonnons votre contenu pour qu'ils soit incroyable. ifzeoijfoij foizejfoizejf ozejf oizje foijz
-                  ofjzoijfz ozj ofizej foizjef ozijf oizej oizje foijze fozje
-                  Nous façonnons votre contenu pour qu'ils soit incroyable. ifzeoijfoij foizejfoizejf ozejf oizje foijz
-                  ofjzoijfz ozj ofizej foizjef ozijf oizej oizje foijze fozje
-                  Nous façonnons votre contenu pour qu'ils soit incroyable. ifzeoijfoij foizejfoizejf ozejf oizje foijz
-                  ofjzoijfz ozj ofizej foizjef ozijf oizej oizje foijze fozje
-                  Nous façonnons votre contenu pour qu'ils soit incroyable.
-              </p>
-          </div>
       </main>
   );
 };
 
 export default AnimatedLogo;
+
+
