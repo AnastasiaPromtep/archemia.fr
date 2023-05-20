ad741b04ed821d45bc81f66b3514faff52eaacc5
Anastasia Prom tep <apromtep@gmail.com>
2023-05-20 17:28:28 +0200

    Add title to hero component

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -4,7 +4,6 @@ import {
     video,
     videoContainer,
     textContainer,
-    subtitle
 } from "./hero.module.css"
 import heroVideo from "../../videos/hero-video.mp4"
 import '../../pages/index.scss';
@@ -16,12 +15,10 @@ const Hero = () => {
                 <video autoPlay muted loop className={video}>
                     <source src={heroVideo} type="video/mp4"/>
                 </video>
-            </div>
-            <div className={textContainer}>
-                <p className={subtitle}>
-                    Une équipe qui vous aide à monter votre stratégie de communication et à créer vos contenus,
-                    sans intermédiaire, en combinant vos besoins et vos envies. L’alchimie parfaite.
-                </p>
+                <div className={ textContainer }>
+                    <h1>ARCHEMIA</h1>
+                    <h2>Idées | Contenu | Alchimie</h2>
+                </div>
             </div>
         </main>
     )
