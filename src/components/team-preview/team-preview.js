e18d45e07cc2e7fb48634e8b4658a41eb9bdd69e
Anastasia Prom tep <apromtep@gmail.com>
2023-05-20 19:03:17 +0200

    Add empty team section

---
+++ src/components/team-preview/team-preview.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {
+    container
+} from './team-preview.module.css';
+
+
+const TeamPreview = () => {
+    return (
+        <main className={ container }>
+            <div>L‘équipe</div>
+        </main>
+    );
+};
+
+export default TeamPreview;
