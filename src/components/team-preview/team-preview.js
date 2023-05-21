5f5a29a548e6d8f5a7a4774b69593bcde6ca86a3
Anastasia Prom tep <apromtep@gmail.com>
2023-05-21 19:14:07 +0200

    Add content to team preview section

--- src/components/team-preview/team-preview.js
+++ src/components/team-preview/team-preview.js
@@ -1,13 +1,33 @@
 import * as React from 'react';
 import {
-    container
+    container,
+    profileContainer,
+    profile
 } from './team-preview.module.css';
+import {StaticImage} from "gatsby-plugin-image";
 
 
 const TeamPreview = () => {
     return (
         <main className={ container }>
-            <div>L‘équipe</div>
+            <h1>L'équipe</h1>
+            <div className={ profileContainer }>
+                <div className={ profile }>
+                    <StaticImage width={200} height={300} src={'../../images/anastasia.JPEG'} alt={'ana image'} />
+                    <h1>Anastasia</h1>
+                    <h2>Prom tep</h2>
+                </div>
+                <div className={ profile }>
+                    <StaticImage width={200} height={300} src={'../../images/calvin.JPEG'} alt={'calvin image'} />
+                    <h1>Calvin</h1>
+                    <h2>Ropers</h2>
+                </div>
+                <div className={ profile }>
+                    <StaticImage width={200} height={300} src={'../../images/thomas.JPEG'} alt={'thomas image'} />
+                    <h1>Thomas</h1>
+                    <h2>Ribier</h2>
+                </div>
+            </div>
         </main>
     );
 };
