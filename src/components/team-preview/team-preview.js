c8822c2daabcb6d4fcd0607152cff81b4e61fb84
Anastasia Prom tep <apromtep@gmail.com>
2023-05-25 16:29:27 +0200

    Add background paintings to team preview

--- src/components/team-preview/team-preview.js
+++ src/components/team-preview/team-preview.js
@@ -2,9 +2,13 @@ import * as React from 'react';
 import {
     container,
     profileContainer,
-    profile
+    profile,
+    video,
+    edition,
+    web
 } from './team-preview.module.css';
 import {StaticImage} from "gatsby-plugin-image";
+import BackgroundPainting from '../../images/background-paintings.svg';
 
 
 const TeamPreview = () => {
@@ -12,17 +16,20 @@ const TeamPreview = () => {
         <main className={ container }>
             <h1>L'équipe</h1>
             <div className={ profileContainer }>
-                <div className={ profile }>
+                <div className={ `${profile} ${web}` }>
+                    <BackgroundPainting />
                     <StaticImage src={'../../images/anastasia.JPEG'} alt={'ana image'} />
                     <h1>Anastasia</h1>
                     <h2>Prom tep</h2>
                 </div>
-                <div className={ profile }>
+                <div className={ `${profile} ${edition}` }>
+                    <BackgroundPainting />
                     <StaticImage src={'../../images/calvin.JPEG'} alt={'calvin image'} />
                     <h1>Calvin</h1>
                     <h2>Ropers</h2>
                 </div>
-                <div className={ profile }>
+                <div className={ `${profile} ${video}` }>
+                    <BackgroundPainting />
                     <StaticImage src={'../../images/thomas.JPEG'} alt={'thomas image'} />
                     <h1>Thomas</h1>
                     <h2>Ribier</h2>
