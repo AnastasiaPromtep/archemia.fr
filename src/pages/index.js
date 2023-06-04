fe3264f2d5f6968c0bef914600535335dbe9c653
Anastasia Prom tep <apromtep@gmail.com>
2023-06-04 23:27:38 +0200

    Remove footer from index component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,25 @@
 import * as React from "react"
+
+import { container } from './index.module.css';
+
 import Layout from "../components/layout";
 import Hero from "../components/hero/hero"
 import Seo from "../components/seo/seo";
-import AnimatedLogo from "../components/animated-logo/animated-logo";
-import { container } from './index.module.css';
 import WorkShowcase from "../components/work-showcase/work-showcase";
 import Contact from "../components/contact/contact";
+import TeamPreview from "../components/team-preview/team-preview";
+import ExpertisePreview from "../components/expertise-preview/expertise-preview";
 
 const IndexPage = () => {
     return (
       <Layout>
         <main id='landingPageContainer' className={ container }>
             <Hero></Hero>
-            <AnimatedLogo></AnimatedLogo>
+            <ExpertisePreview expertise='edition'></ExpertisePreview>
+            <ExpertisePreview expertise='video'></ExpertisePreview>
+            <ExpertisePreview expertise='web'></ExpertisePreview>
             <WorkShowcase></WorkShowcase>
+            <TeamPreview></TeamPreview>
             <Contact></Contact>
         </main>
       </Layout>
