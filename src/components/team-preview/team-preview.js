abb109fa206e33a55c59a2c4b5dd822702651d13
Anastasia Prom tep <apromtep@gmail.com>
2023-06-04 15:54:13 +0200

    Change wording style

--- src/components/team-preview/team-preview.js
+++ src/components/team-preview/team-preview.js
@@ -14,25 +14,34 @@ import BackgroundPainting from '../../images/background-paintings.svg';
 const TeamPreview = () => {
     return (
         <main className={ container }>
-            <h1>L'équipe</h1>
+            <h1>Nos 3 pôles</h1>
             <div className={ profileContainer }>
                 <div className={ `${profile} ${web}` }>
                     <BackgroundPainting />
                     <StaticImage src={'../../images/anastasia.JPEG'} alt={'ana image'} />
-                    <h1>Anastasia</h1>
-                    <h2>Prom tep</h2>
+                    <div>
+                        <h1>Anastasia</h1>
+                        <h2>PROM TEP</h2>
+                        <span>Directrice technique</span>
+                    </div>
                 </div>
                 <div className={ `${profile} ${edition}` }>
                     <BackgroundPainting />
                     <StaticImage src={'../../images/calvin.JPEG'} alt={'calvin image'} />
-                    <h1>Calvin</h1>
-                    <h2>Ropers</h2>
+                    <div>
+                        <h1>Calvin</h1>
+                        <h2>ROPERS</h2>
+                        <span>Directeur éditorial</span>
+                    </div>
                 </div>
                 <div className={ `${profile} ${video}` }>
                     <BackgroundPainting />
                     <StaticImage src={'../../images/thomas.JPEG'} alt={'thomas image'} />
-                    <h1>Thomas</h1>
-                    <h2>Ribier</h2>
+                    <div>
+                        <h1>Thomas</h1>
+                        <h2>RIBIER</h2>
+                        <span>Directeur audiovisuelle</span>
+                    </div>
                 </div>
             </div>
         </main>
