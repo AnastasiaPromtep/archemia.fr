72c73d8fb32018b9f1dfcc0e67b61fa8c97eb800
Anastasia Prom tep <apromtep@gmail.com>
2023-06-06 12:03:47 +0200

    Add team page

---
+++ src/pages/team/index.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Layout from "../../components/layout";
+
+const TeamPage = () => {
+
+    return (
+        <Layout>
+            <main>
+                <div>
+                    Under construction
+                </div>
+            </main>
+        </Layout>
+    );
+};
+
+export default TeamPage;
