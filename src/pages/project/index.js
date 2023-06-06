4887ee9c401feb0d8ffb530b3b1f222f010a0fcd
Anastasia Prom tep <apromtep@gmail.com>
2023-06-06 12:04:18 +0200

    Add Project page

---
+++ src/pages/project/index.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+import Layout from "../../components/layout";
+
+const ProjectPage = () => {
+
+    return (
+        <Layout>
+            <main>
+                <div>
+                    Under construction
+                </div>
+            </main>
+        </Layout>
+    );
+};
+
+export default ProjectPage;
